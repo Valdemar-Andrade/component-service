bb1bebc5eea67c5a97ae9585df9c7195f0b1bbe9
Valdemar Andrade <andradevaldemar298@gmail.com>
2026-05-07 21:37:02 +0100

    refactor: adicionado lógica no KafkaConsumer para validar o objeto

--- src/main/java/com/projeto/componentservice/kafka/KafkaProcessedMaterialConsumer.java
+++ src/main/java/com/projeto/componentservice/kafka/KafkaProcessedMaterialConsumer.java
@@ -2,6 +2,8 @@ package com.projeto.componentservice.kafka;
 
 import com.projeto.componentservice.dto.BaseEvent;
 import com.projeto.componentservice.service.ComponentService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
@@ -9,6 +11,7 @@ import org.springframework.stereotype.Service;
 public class KafkaProcessedMaterialConsumer {
 
     private final ComponentService componentService;
+    private static final Logger logger = LoggerFactory.getLogger(KafkaProcessedMaterialConsumer.class);
 
     public KafkaProcessedMaterialConsumer(ComponentService componentService) {
         this.componentService = componentService;
@@ -16,8 +19,16 @@ public class KafkaProcessedMaterialConsumer {
 
     @KafkaListener(topics = "industria-carro", groupId = "component-group")
     public void consume(BaseEvent event) {
-        if ("MATERIAL_PROCESSED".equals(event.eventType())) {
-            componentService.handleMaterial(event);
+        try {
+            if ("MATERIAL_PROCESSED".equals(event.eventType())) {
+                logger.info("[COMPONENT-SERVICE] Processando material transformado: {}", event.eventId());
+                componentService.handleMaterial(event);
+            } else {
+                logger.debug("[COMPONENT-SERVICE] Evento ignorado: {}", event.eventType());
+            }
+        } catch (Exception e) {
+            logger.error("[COMPONENT-SERVICE] Erro ao processar evento: {}", e.getMessage());
         }
     }
+
 }
